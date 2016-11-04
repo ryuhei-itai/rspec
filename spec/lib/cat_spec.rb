8bf091fa2ec31ace9521487de7b141370b4ba059
ryuhei-itai <dev.classic88@gmail.com>
2016-11-04 18:54:34 +0900

    江草氏レビュー

--- spec/lib/cat_spec.rb
+++ spec/lib/cat_spec.rb
@@ -3,60 +3,59 @@ require "cat"
 
 describe Cat do
 
-  before(:all) do
-    # インスタンスを生成する
+  # インスタンスを生成する
 
-    @tama = Cat.new("tama", Cat::YOUNG)
-    @neko = Cat.new(:neko, Cat::OLD)
+  let(:tama) { Cat.new('tama', Cat::YOUNG) }
+  let(:neko) { Cat.new(:neko, Cat::OLD) }
 
-    # クラスメソッドの呼び出し
+  # クラスメソッドの呼び出し
 
-    # 内部的にインスタンスを生成する
-    @zizi = Cat.young("zizi")
-    # インスタンスを生成しない
-    @kiki = Cat.old("kiki")
-  end
+  # 内部的にインスタンスを生成する
+  let(:zizi) { Cat.young("zizi") }
+  # インスタンスを生成しない
+  let(:kiki) { Cat.old("kiki") }
 
   describe "name" do
     it "is 'tama'" do
-      expect(@tama.name).to eq 'tama'
+      expect(tama.name).to eq 'tama'
     end
     it "is 'neko'" do
-      expect(@neko.name.to_s).to eq 'neko'
+      expect(neko.name.to_s).to eq 'neko'
     end
     it "is 'zizi'" do
-      expect(@zizi.name).to eq 'zizi'
+      expect(zizi.name).to eq 'zizi'
     end
     it "is not 'kiki'" do
       # attr_accessorメソッドはインスタンス変数を定義するので読み込めない
       # 代わりにclass.nameは名前空間を取得する
-      expect(@kiki.name).to eq 'Cat'
+      expect(kiki.name).to eq 'Cat'
     end
   end
 
   describe "older" do
     it "is not 'old'" do
       # attr_accessorメソッドはインスタンス変数を定義するので読み込めない
-      expect{ @kiki.older }.to raise_error(NoMethodError)
+      expect{ kiki.older }.to raise_error(NoMethodError)
     end
   end
 
   describe "hello" do
-    context "is 'nyao!!'" do
+    # contextはifに相当する
+    context "is young" do
       it "by tama" do
-        expect(@tama.hello).to eq 'nyao!!'
+        expect(tama.hello).to eq 'nyao!!'
       end
       it "by zizi" do
-        expect(@zizi.hello).to eq 'nyao!!'
+        expect(zizi.hello).to eq 'nyao!!'
       end
     end
-    context "is 'nya'" do
+    context "is old" do
       it "by neko" do
-        expect(@neko.hello).to eq 'nya'
+        expect(neko.hello).to eq 'nya'
       end
       it "by kiki" do
         # インスタンスを生成していないので、インスタンスメソッドは読み込めない
-        expect{ @kiki.hello }.to raise_error(NoMethodError)
+        expect{ kiki.hello }.to raise_error(NoMethodError)
       end
     end
   end
